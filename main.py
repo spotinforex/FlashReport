5b479005c75eec71f5532a9b596cb855c5f75028
Praisejah <nwabekepraisejah@gmail.com>
2025-12-31 13:23:35 +0100

    Created Scraper For One News Source

---
+++ main.py
@@ -0,0 +1,19 @@
+from playwright.async_api import async_playwright 
+import  asyncio
+from scrapper.adapter.punch import parse_punch_news, save_to_json
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+
+        await page.goto("https://punchng.com", wait_until = "domcontentloaded")
+        content = await page.content()
+        print(content)
+
+        parsed = parse_punch_news(content)
+        save_to_json(parsed)
+
+        await browser.close()
+
+asyncio.run(main())
